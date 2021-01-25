bc0abb192a9c6bc194c8995255be8ad607c317f0
SinghJai13 <46139190+SinghJai13@users.noreply.github.com>
2021-01-26 00:41:12 +0530

    Longest common-repeating subsequence
    str[i-1]==str[j-1] then i!=j
    same as LCS

---
+++ Strings/longest_repeating_subsequence.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+	int LongestRepeatingSubsequence(string str){
+		    // Code here
+		    int n=str.length()+1;
+		    int dp[n][n];
+		    
+		    for(int i=0;i<n;i++)
+		    {
+		        for(int j=0;j<n;j++)
+		        {
+		            dp[i][j]=0;
+		        }
+		    }
+		    
+		    for(int i=1;i<n;i++)
+		    {
+		        for(int j=1;j<n;j++)
+		        {
+		            if(str[i-1]==str[j-1]&&i!=j)
+		            {
+		                dp[i][j]=1+dp[i-1][j-1];
+		            }
+		            else
+		            {
+		                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+		            }
+		        }
+		    }
+		    return dp[n-1][n-1];
+		}
+
+int main()
+{
+    string str;cin>>str;
+
+    cout<<"length of longest repeating subsequence: "<<LongestRepeatingSubsequence(str)<<endl;
+}
